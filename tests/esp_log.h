362eaa89ca7b059aade1ff0fe57f3d0618b7b239
bledaburat97 <110774799+bledaburat97@users.noreply.github.com>
2025-06-16 13:03:20 +0300

    Add comprehensive BLE message flow tests

---
+++ tests/esp_log.h
@@ -0,0 +1,7 @@
+#ifndef ESP_LOG_H
+#define ESP_LOG_H
+#include <stdio.h>
+#define ESP_LOGI(tag, fmt, ...) ((void)0)
+#define ESP_LOGE(tag, fmt, ...) ((void)0)
+#define ESP_LOGW(tag, fmt, ...) ((void)0)
+#endif
