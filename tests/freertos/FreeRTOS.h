29fe18c7546efff0c6e17af5cbcec3a480237143
bledaburat97 <110774799+bledaburat97@users.noreply.github.com>
2025-06-16 16:04:35 +0300

    Extract message handling logic for testing

---
+++ tests/freertos/FreeRTOS.h
@@ -0,0 +1,7 @@
+#ifndef FREERTOS_FREERTOS_H
+#define FREERTOS_FREERTOS_H
+#define pdTRUE 1
+#define pdFALSE 0
+typedef unsigned int TickType_t;
+#define pdMS_TO_TICKS(x) (x)
+#endif
--- tests/test_message_flow.c
+++ tests/test_message_flow.c
@@ -9,53 +9,13 @@
     printf("Assertion failed: %d != %d\n", (expected),(actual)); return 1; }
 #define TEST_ASSERT_TRUE(cond) if(!(cond)){ printf("Assertion failed: %s\n", #cond); return 1; }
 
-// === Begin stubs and structures ===
+// === Begin stubs and state ===
 
-typedef enum {
-    STATE_TEMPERATURE_ALERT,
-    STATE_HUMIDITY_ALERT,
-    STATE_ACTIVE,
-    STATE_INACTIVITY,
-    STATE_START,
-    STATE_IDLE
-} DeviceState;
-
-typedef enum {
-    THERAPY_STARTED_BY_APP,
-    THERAPY_PAUSED_BY_APP,
-    THERAPY_CONTINUED_BY_APP,
-    THERAPY_STOPPED_BY_APP,
-    REGIONS_BRIGHTNESS_UPDATED,
-    CURRENT_STATE_THERAPY,
-    CURRENT_STATE_INACTIVITY,
-    CURRENT_STATE_TEMP_ALERT,
-    CURRENT_STATE_HUM_ALERT,
-    HELMET_ON,
-    HELMET_OFF,
-    ONGOING_THERAPY
-} NotificationType;
-
-typedef enum {
-    NOTIFICATION_INFO_MESSAGE,
-    ACTIVE_THERAPY_INFO_MESSAGE,
-    MEASUREMENT_INFO_MESSAGE,
-    DEVICE_INFO_MESSAGE,
-    RECORDS_INFO_MESSAGE
-} MessageType;
-
-typedef struct {
-    uint16_t therapy_duration;
-    uint8_t brightness[6];
-} ActivationMessage;
-
-typedef struct {
-    char type[10];
-    uint16_t therapy_id;
-} StatusChangeMessage;
-
-typedef struct {
-    uint8_t type;
-} FeedbackMessage;
+#include "../main/transaction_logic.h"
+#include "../main/laser_driver_control.h"
+#include "../main/timer_management.h"
+#include "../main/state_manager.h"
+#include "../main/transaction_manager.h"
 
 static DeviceState current_state;
 static bool helmet_on = true;
@@ -69,6 +29,7 @@ bool get_helmet_state() { return helmet_on; }
 uint8_t get_temperature() { return 25; }
 uint16_t read_therapy_count() { return 1; }
 void start_new_therapy(uint16_t duration) { (void)duration; }
+void set_brightness_of_region(uint8_t r, uint8_t b){ (void)r; (void)b; }
 void start_therapy(bool cont) { (void)cont; }
 void stop_lasers() {}
 void update_passed_therapy_duration() {}
@@ -90,60 +51,6 @@ void add_and_send_notification_info(NotificationType type){
 }
 
 // === Code under test ===
-static void handle_activation_message(const ActivationMessage* msg){
-    for(int i=0;i<6;i++){ /* set brightness stubbed */ }
-    if(msg->therapy_duration>0){
-        if(get_device_state()==STATE_INACTIVITY || get_device_state()==STATE_ACTIVE){
-            if(get_helmet_state()){
-                start_new_therapy(msg->therapy_duration);
-                add_and_send_notification_info(THERAPY_STARTED_BY_APP);
-            }
-        }
-    }else if(get_device_state()==STATE_ACTIVE){
-        add_and_send_notification_info(REGIONS_BRIGHTNESS_UPDATED);
-    }
-}
-
-static void handle_status_change_message(const StatusChangeMessage* msg){
-    if(strcmp(msg->type,"STOP")==0){
-        if(get_device_state()==STATE_ACTIVE){
-            add_and_send_notification_info(THERAPY_STOPPED_BY_APP);
-            stop_lasers();
-            reset_passed_therapy_duration();
-        }
-    } else if(strcmp(msg->type,"PAUSE")==0){
-        if(get_device_state()==STATE_ACTIVE){
-            add_and_send_notification_info(THERAPY_PAUSED_BY_APP);
-            stop_lasers();
-            update_passed_therapy_duration();
-        }
-    } else if(strcmp(msg->type,"CONTINUE")==0){
-        if(get_device_state()==STATE_INACTIVITY){
-            start_therapy(true);
-        }
-    }
-}
-
-static void handle_feedback_message(const FeedbackMessage* msg){
-    if(msg->type==0){ // DEVICE_INFO_MESSAGE_ACK
-        NotificationType n;
-        if(get_device_state()==STATE_ACTIVE) n=CURRENT_STATE_THERAPY;
-        else if(get_device_state()==STATE_INACTIVITY) n=CURRENT_STATE_INACTIVITY;
-        else if(get_device_state()==STATE_TEMPERATURE_ALERT) n=CURRENT_STATE_TEMP_ALERT;
-        else if(get_device_state()==STATE_HUMIDITY_ALERT) n=CURRENT_STATE_HUM_ALERT; else return;
-        add_and_send_notification_info(n);
-    } else if(msg->type==1){ // DEVICE_STATE_INFO_MESSAGE_ACK
-        add_and_send_notification_info(get_helmet_state()?HELMET_ON:HELMET_OFF);
-    } else if(msg->type==2){ // HELMET_STATE_INFO_MESSAGE_ACK
-        if(get_device_state()==STATE_ACTIVE) {
-            add_and_send_notification_info(ONGOING_THERAPY);
-        } else {
-            // send measurement info
-            uint8_t buf[3]={get_temperature(),0,0};
-            send_info_message(MEASUREMENT_INFO_MESSAGE, buf, 3);
-        }
-    }
-}
 
 // === Simple fragment encoder from matching_message_encoder.c ===
 #include "../main/matching_message_encoder.c"
@@ -177,12 +84,12 @@ int main(){
     TEST_ASSERT_EQUAL_INT(THERAPY_STOPPED_BY_APP,last_notification);
 
     // feedback device info ack when active
-    FeedbackMessage fb={0}; current_state=STATE_ACTIVE; last_notification=0;
+    FeedbackMessage fb={.type=DEVICE_INFO_MESSAGE_ACK}; current_state=STATE_ACTIVE; last_notification=0;
     handle_feedback_message(&fb);
     TEST_ASSERT_EQUAL_INT(CURRENT_STATE_THERAPY,last_notification);
 
     // feedback helmet state ack when helmet off
-    fb.type=1; helmet_on=false; current_state=STATE_INACTIVITY; last_notification=0;
+    fb.type=DEVICE_STATE_INFO_MESSAGE_ACK; helmet_on=false; current_state=STATE_INACTIVITY; last_notification=0;
     handle_feedback_message(&fb);
     TEST_ASSERT_EQUAL_INT(HELMET_OFF,last_notification);
     helmet_on=true;
